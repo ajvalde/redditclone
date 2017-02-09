3f730ae5b18c04d504d56a48a63627f9b771b9f0
Anthony <anthonyguitarman13@gmail.com>
2017-02-08 20:14:24 -0700

    coded my life away

--- app/app.js
+++ app/app.js
@@ -1,1 +1,30 @@
-var redditClone = angular.moduel('clone', []);
+(function() {
+
+    angular.module('clone', [])
+        .component('page', {
+            templateUrl: 'views/body.html',
+            controller: controller
+        })
+
+    function controller() {
+        const vm = this
+
+        vm.$onInit = function() {
+
+          vm.posts = [
+
+          ]
+
+            vm.showForm = function() {
+                vm.addForm = !vm.addForm
+            }
+
+            vm.postPost = function() {
+              vm.posts.push(vm.post)
+              vm.showForm();
+              delete vm.post
+            }
+        }
+    }
+
+}());
