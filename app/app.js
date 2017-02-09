802897400c5aaec8afa3a63b8a60796b5c58e86b
Anthony <anthonyguitarman13@gmail.com>
2017-02-09 15:23:16 -0700

    adding comments

--- app/app.js
+++ app/app.js
@@ -12,7 +12,13 @@
         vm.$onInit = function() {
 
           vm.posts = [
-
+            {
+              title: "Teemo",
+              body: "Teemo is evil!!!!!",
+              author: "Me",
+              image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRtraHbHZEf1FhP2usWP9XAgZso1Fr8lbD_FHlacCtHIAAfK3JlQg",
+              comments: []
+            }
           ]
 
             vm.showForm = function() {
@@ -20,9 +26,19 @@
             }
 
             vm.postPost = function() {
+              vm.post.comments =[]
               vm.posts.push(vm.post)
               vm.showForm();
               delete vm.post
+            }
+
+            vm.writeComment = function() {
+              vm.showCom = !vm.showCom
+            }
+
+            vm.postComment = function(post) {
+              post.comments.push(post.newComment)
+              delete post.newComment
             }
         }
     }
